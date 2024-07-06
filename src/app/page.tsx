8748cfe9746d9712c0b205b13a10fad3917ad785
Jake Lee <freejak5520@gmail.com>
2024-07-06 15:21:14 +0900

    디자인 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,13 +12,13 @@ export default function Home() {
   }, [theme]);
 
   return (
-    <main>
-      <button className="text-neutral-950 dark:text-neutral-50" onClick={() => {
+    <main className="flex flex-col justify-center items-center min-h-dvh gap-6">
+      <div className="text-neutral-950 dark:text-neutral-50">
+        Now theme : {`${nowTheme || "Default"}`}
+      </div>
+      <button className="border border-neutral-700 dark:border-neutral-300 rounded-full px-8 py-1 text-neutral-950 dark:text-neutral-50" onClick={() => {
         setTheme(theme === "dark" ? "" : "dark");
-      }}>Up</button>
-      <span className="text-neutral-950 dark:text-neutral-50">
-        {`${nowTheme}`}
-      </span>
+      }}>Toggle</button>
     </main>
   );
 }
